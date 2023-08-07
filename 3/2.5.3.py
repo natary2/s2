2103a710cd0a062008370e40541312d7ea11d580
natary2 <140882798+natary2@users.noreply.github.com>
2023-08-07 20:12:01 +0300

    Add files via upload

---
+++ 3/2.5.3.py
@@ -0,0 +1,8 @@
+summa=int(input("Минимальная сумма - "))
+maikl=int(input("Cколько у Майкла - "))
+ivan=int(input("Сколько у Ивана - "))
+if (maikl>=summa) and (ivan>=summa):    print(2)
+elif (maikl>=summa) and (ivan<=summa):  print("Mike")
+elif (maikl<=summa) and (ivan>=summa):  print("Ivan")
+elif (maikl<=summa) and (ivan<=summa) and ((maikl+ivan)>=summa):    print(1)
+elif (maikl<=summa) and (ivan<=summa) and ((maikl+ivan)<=summa):    print(0)
